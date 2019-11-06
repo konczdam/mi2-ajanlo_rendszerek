20fb7c0eefa3bf367ab9f3e7dad017478979191a
Adam Koncz <konczdam98@gmail.com>
2019-11-06 15:18:28 +0100

    alakul

---
+++ src/main/java/Coordinate.java
@@ -0,0 +1,22 @@
+public class Coordinate {
+
+    private int i,j;
+
+    public Coordinate(int i, int j) {
+        this.i = i;
+        this.j = j;
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public int getJ() {
+        return j;
+    }
+
+    @Override
+    public String toString() {
+        return "i: " + i + " j: " + j;
+    }
+}
--- src/main/java/Solver.java
+++ src/main/java/Solver.java
@@ -13,14 +13,19 @@ public class Solver {
 
     private  int numOfRatings, numOfUsers, numOfMovies, K;
     private List<User> users;
-    RealMatrix matrix;
+    private List<Coordinate> presentValuesInOriginalMatrix;
+    RealMatrix originalMatrix;
+    private final double epsilon = 0.25;
+    private static final double DOUBLE_COMPARE_EPSILON = 0.0001;
+    private double acceptedTotalError;
+    private RealMatrix canditateMatrix;
 
     public Solver(InputStream inputStream){
         Scanner scanner = new Scanner(inputStream);
         initAttributes(scanner);
 
-        matrix = new Array2DRowRealMatrix(numOfUsers,numOfMovies);
-        initMatrix(scanner, matrix);
+        originalMatrix = new Array2DRowRealMatrix(numOfUsers,numOfMovies);
+        initMatrix(scanner, originalMatrix);
 
 
     }
@@ -32,6 +37,8 @@ public class Solver {
         users = new ArrayList<>(numOfUsers);
         IntStream.range(0,numOfUsers).forEach(m -> users.add(new User()));
         K = new Double(numOfUsers / 2.5).intValue();
+        acceptedTotalError = numOfUsers * numOfRatings * 0.01;
+        presentValuesInOriginalMatrix = new ArrayList<>(numOfRatings);
     }
 
     private void initMatrix(Scanner scanner, RealMatrix matrix) {
@@ -41,6 +48,7 @@ public class Solver {
             int rating = scanner.nextInt();
             matrix.setEntry(userIndex, movieIndex, rating);
             users.get(userIndex).addRatedMovie(movieIndex);
+            presentValuesInOriginalMatrix.add(new Coordinate(userIndex, movieIndex));
         }
     }
 
@@ -51,10 +59,18 @@ public class Solver {
         initMatrixWithRandomValues(P);
         initMatrixWithRandomValues(Q);
 
+
+        while(true){
+            canditateMatrix = P.multiply(Q);
+            if(!isTotalErrorTooBig())
+                break;
+            editMatrices(P,Q);
+        }
+
         System.out.println(P);
         System.out.println(Q);
 
-        
+
         System.out.println(P.multiply(Q));
     }
 
@@ -62,11 +78,35 @@ public class Solver {
         int columns = p.getColumnDimension();
         int rows = p.getRowDimension();
 
+        ThreadLocalRandom tlr = ThreadLocalRandom.current();
+
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
-                p.setEntry(i,j, ThreadLocalRandom.current().nextInt(5));
+                p.setEntry(i,j, tlr.nextInt(5));
             }
 
         }
     }
+
+    private boolean isTotalErrorTooBig() {
+        double totalError = getTotalError();
+        return totalError > acceptedTotalError;
+    }
+
+    private double getTotalError() {
+        double res = 0.0;
+
+
+        for(Coordinate c: presentValuesInOriginalMatrix){
+            double entry = originalMatrix.getEntry(c.getI(), c.getJ());
+            res += Math.pow(entry - canditateMatrix.getEntry(c.getI(),c.getJ()) , 2);
+        }
+
+        return res;
+
+    }
+
+    private void editMatrices(RealMatrix P, RealMatrix Q) {
+    }
+
 }
